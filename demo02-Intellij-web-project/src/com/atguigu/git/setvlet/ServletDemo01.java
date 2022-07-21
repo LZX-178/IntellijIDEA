c619ad0967adc415cb2fe778f344ed7b8b233f3a
LZXhp <LZXhp@qq.com>
2022-07-21 11:21:28 +0800

    test

---
+++ demo02-Intellij-web-project/src/com/atguigu/git/setvlet/ServletDemo01.java
@@ -0,0 +1,11 @@
+package com.atguigu.git.setvlet;
+
+/**
+ * @author LZX
+ * @create 2022-07-21 11:10
+ */
+public class ServletDemo01 {
+    public static void main(String[] args) {
+        System.out.println("servlet");
+    }
+}
